637d9ec1124d86ce5cfa1bb2147fb33a8d4b7511
dan-db-berg <dan@hiro-media.com>
2017-09-03 13:26:25 +0000

    files

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,46 @@
+package "nginx.x86_64" do
+      action :install
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
+
+file '/etc/nginx/conf.d/default.conf' do
+  action :delete
+end
+
+file '/etc/nginx/conf.d/default.conf' do
+  content 'server {
+       listen         80;
+       server_name    sync.msv-a.net;
+       return         301 https://$server_name$request_uri;
+}
+
+server {
+listen  443;
+ssl on;
+ssl_certificate_key /etc/ssl/sync.msv-a.net.key;
+ssl_certificate /etc/ssl/chain.crt;
+
+server_name sync.msv-a.net;
+root /usr/share/tomcat/webapps/intuit/;
+
+  location / {
+    proxy_set_header X-Forwarded-Host $host;
+    proxy_set_header X-Forwarded-Server $host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_pass http://127.0.0.1:8080/intuit/;
+  }
+}'
+end
+
+bash "certificates_copy" do
+  code <<-EOL
+  cp /root/chef-repo/cookbooks/nginx/files/* /etc/ssl/
+  EOL
+end
+
+service "nginx" do
+  action :restart
+end
