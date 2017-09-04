3064612a7bcff3355f91f69e4dc20100a6389f36
dan-db-berg <dan@hiro-media.com>
2017-09-04 15:24:51 +0000

    yeah

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -1,20 +1,29 @@
 remote_file "/tmp/apache-tomcat-7.0.67.zip" do
-	source "http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.67/bin/apache-tomcat-7.0.67.zip"
+        source "https://s3.amazonaws.com/cf-soft/apache-tomcat-7.0.67.zip"
 end
 
-bash "unpack_tomcat" do
-	code <<-EOL
-	unzip /tmp/apache-tomcat-7.0.67.zip -d /opt/
-	mv /opt/apache-tomcat-7.0.67 /opt/tomcat/
-	chown -R tomcat:tomcat /opt/tomcat/
-	chmod 0755 /opt/tomcat/bin/*.sh
-	EOL
+package "unzip.x86_64" do
+      action :install
 end
 
 user 'tomcat' do
   manage_home true
   home '/home/tomcat'
   shell '/bin/bash'
+end
+
+bash "unpack_tomcat" do
+        code <<-EOL
+        unzip /tmp/apache-tomcat-7.0.67.zip -d /opt/
+        mv /opt/apache-tomcat-7.0.67 /opt/tomcat/
+        EOL
+end
+
+bash "tomcat_permissions" do
+        code <<-EOL
+        chown -R tomcat:tomcat /opt/tomcat/
+        chmod 0755 /opt/tomcat/bin/*.sh
+        EOL
 end
 
 template "tomcat" do
@@ -24,12 +33,6 @@ template "tomcat" do
   group "root"
   mode "0755"
 end
-
-service "tomcat" do
-    supports :restart => true, :start => true, :stop => true, :reload => true
-    action [ :enable, :start ]
-    subscribes :restart, "template[tomcat]", :immediately
-end 
 
 directory "/opt/tomcat/webapps/intuit" do
   mode 0755
@@ -42,4 +45,10 @@ file '/opt/tomcat/webapps/intuit/index.html' do
   content 'Hello Intutit'
   owner 'tomcat'
   group 'tomcat'
+end
+
+service "tomcat" do
+    supports :restart => true, :start => true, :stop => true, :reload => true
+    action [ :enable, :start ]
+    subscribes :restart, "template[tomcat]", :immediately
 end
