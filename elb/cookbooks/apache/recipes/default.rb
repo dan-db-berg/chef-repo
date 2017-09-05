879d65379c78d3490e0c2ca8464ec09c59745fc0
dan-db-berg <dan@hiro-media.com>
2017-09-05 21:36:41 +0000

    b

--- elb/cookbooks/apache/recipes/default.rb
+++ elb/cookbooks/apache/recipes/default.rb
@@ -2,15 +2,7 @@ package "httpd.x86_64" do
       action :install
 end
 
-template "worker" do
-  path "/etc/httpd/"
-  source "workers.properties.erb"
-  owner "root"
-  group "root"
-  mode "0755"
-end
-
-template "worker" do
+template "workers.properties" do
   path "/etc/httpd/conf/"
   source "workers.properties.erb"
   owner "root"
@@ -18,7 +10,7 @@ template "worker" do
   mode "0755"
 end
 
-template "httpd" do
+template "httpd.conf" do
   path "/etc/httpd/conf/"
   source "httpd.conf.erb"
   owner "root"
@@ -26,8 +18,8 @@ template "httpd" do
   mode "0755"
 end
 
-template "modjk" do
-  path "/etc/httpd/modules"
+template "mod_jk.so" do
+  path "/etc/httpd/modules/"
   source "mod_jk.so.erb"
   owner "root"
   group "root"
